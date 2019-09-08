1c894869c523cf0bb5769cf9b20f52bbbf89f3d0
Willian Hua <huawillian@gmail.com>
2019-09-07 21:54:02 -0400

    Added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,59 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CodeComponent } from './pages/code/code.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CodeDetailComponent } from './pages/code-detail/code-detail.component';
+import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
+import { BlogDetailComponent } from './pages/blog-detail/blog-detail.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'code',
+    component: CodeComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'blog',
+    component: BlogsComponent
+  }, 
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'code/:id',
+    component: CodeDetailComponent
+  },
+  {
+    path: 'projects/:id',
+    component: ProjectDetailComponent
+  },
+  {
+    path: 'blog/:id',
+    component: BlogDetailComponent
+  },
+  {
+    path: '*',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CodeComponent } from './pages/code/code.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
+import { BlogDetailComponent } from './pages/blog-detail/blog-detail.component';
+import { CodeDetailComponent } from './pages/code-detail/code-detail.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    CodeComponent,
+    ProjectsComponent,
+    BlogsComponent,
+    ContactComponent,
+    ProjectDetailComponent,
+    BlogDetailComponent,
+    CodeDetailComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
