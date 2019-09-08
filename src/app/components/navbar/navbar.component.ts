f163bc156f01d745847f969ef046452cd4bf1caa
Willian Hua <huawillian@gmail.com>
2019-09-08 15:39:34 -0400

    Fine tune navbar and home hero banner

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -8,13 +9,50 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   public expanded: boolean = false;
+  public showNavbarBg: boolean = false;
+  private isRaf: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private elementRef: ElementRef) { }
 
   ngOnInit() {
     this.router.events.subscribe(() => {
       this.expanded = false;
     });
+
+    fromEvent(window, 'click').subscribe((event) => {
+      if (this.expanded && !this.isChildElement(event.target, this.elementRef.nativeElement)) {
+        this.expanded = false;
+      }
+    });
+    this.handleScroll();
+
+    fromEvent(window, 'scroll').subscribe(() => {
+      if (!this.isRaf) {
+        this.isRaf = true;
+        window.requestAnimationFrame(this.handleScroll.bind(this));
+      }
+    });
+  }
+
+  handleScroll() {
+    if (this.isRaf) {
+      this.showNavbarBg = window.scrollY > 100;
+      this.isRaf = false;
+    }
+  }
+
+  private isChildElement(childEl, parentEl): boolean {
+    let currEl = childEl;
+
+    do {
+      if (currEl === parentEl) {
+        return true;
+      } else {
+        currEl = currEl.parentNode;
+      }
+    } while (currEl);
+
+    return false;
   }
 
   public toggle() {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  @ViewChild('bgImg', { static: true }) bgImg;
+  private scrollSubscription: Subscription;
+  private isRaf = false;
+
+  public codingExpanded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.handleScroll();
+
+    this.scrollSubscription = fromEvent(window, 'scroll').subscribe(() => {
+      if(!this.isRaf) {
+        this.isRaf = true;
+        window.requestAnimationFrame(this.handleScroll.bind(this));
+      }
+    });
   }
 
+  handleScroll() {
+    if(this.isRaf) {
+      this.bgImg.nativeElement.style.transform = `translateY(-${window.scrollY / 3}px)`;
+      this.isRaf = false;
+    }
+  }
+
+  expandCode() {
+    this.codingExpanded = true;
+  }
+
+  ngOnDestroy() {
+    this.scrollSubscription.unsubscribe();
+  }
 }
