76d17b2ae86c57b0024c524c65fc3399b75c36dd
Willian Hua <huawillian@gmail.com>
2019-09-12 00:54:06 -0400

    Added contacts page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  @ViewChild('bgImg', { static: true }) bgImg;
+  private scrollSubscription: Subscription;
+  private isRaf = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.handleScroll();
+
+    this.scrollSubscription = fromEvent(window, 'scroll').subscribe(() => {
+      if (!this.isRaf) {
+        this.isRaf = true;
+        window.requestAnimationFrame(this.handleScroll.bind(this));
+      }
+    });
   }
 
+  handleScroll() {
+    if (this.isRaf) {
+      this.bgImg.nativeElement.style.transform = `translateY(-${window.scrollY / 3}px)`;
+      this.isRaf = false;
+    }
+  }
+
+  ngOnDestroy() {
+    this.scrollSubscription.unsubscribe();
+  }
 }
